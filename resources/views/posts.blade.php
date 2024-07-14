676c4c8592df2c3b392295fb710430b34bb4f433
@muhammad-ari-kuncoro <@muhammad-ari-kuncoro>
2024-07-14 12:53:57 +0700

    layouting
    layouting blade

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+
+@section('container')
+@foreach ($posts as $post )
+<article class="mb-3 mt-3">
+
+    <h1>
+        <a href="/post/{{ $post["slug"] }}">{{ $post["title"] }}</a></h1>
+    <h2>By: {{ $post["author"] }}</h2>
+    <p>{{ $post["body"] }}</p>
+</article>
+@endforeach
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,89 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',[
+        'title_navbar' => 'Home',
+        'title' => 'Home Page'
+    ]);
+});
+
+Route::get('/posts', function () {
+
+
+$blog_posts = [
+
+    [
+     "title" => "judul Post Pertama",
+     "slug" => 'Judul_Post_Pertama',
+     "author" => "Muhammad ari kuncoro",
+     "body" => "
+     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis dolore repudiandae rerum fuga voluptate hic nihil accusantium, eligendi beatae corporis odio ab laboriosam eum, eveniet facere dignissimos explicabo facilis qui minima similique quisquam. Dolores sint dicta deleniti nemo quam ratione, tempore assumenda saepe vero expedita eius laborum, nesciunt tempora suscipit?"
+    ] ,
+
+    [
+     "title" => "judul Post Kedua",
+     "slug" => 'Judul_Post_Kedua',
+     "author" => "Muhammad ari ",
+     "body" => "
+     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis dolore repudiandae rerum fuga voluptate hic nihil accusantium, eligendi beatae corporis odio ab laboriosam eum, eveniet facere ?"
+    ] ,
+
+ ];
+
+
+    return view('posts',[
+        'title_navbar' => 'Blog',
+        'title' => 'Blog Page',
+        'posts' => $blog_posts
+
+    ]);
+});
+
+
+
+Route::get('/about', function () {
+    return view('about' ,[
+        'title_navbar' => 'About',
+        'title' => 'About Page',
+    ]);
+});
+
+
+
+// Single Page Blog Posts
+Route::get('post/{slug}', function($slug){
+
+    $blog_posts = [
+
+        [
+         "title" => "judul Post Pertama",
+         "slug" => 'Judul_Post_Pertama',
+         "author" => "Muhammad ari kuncoro",
+         "body" => "
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis dolore repudiandae rerum fuga voluptate hic nihil accusantium, eligendi beatae corporis odio ab laboriosam eum, eveniet facere dignissimos explicabo facilis qui minima similique quisquam. Dolores sint dicta deleniti nemo quam ratione, tempore assumenda saepe vero expedita eius laborum, nesciunt tempora suscipit?"
+        ] ,
+
+        [
+         "title" => "judul Post Kedua",
+         "slug" => 'Judul_Post_Kedua',
+         "author" => "Muhammad ari ",
+         "body" => "
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis dolore repudiandae rerum fuga voluptate hic nihil accusantium, eligendi beatae corporis odio ab laboriosam eum, eveniet facere ?"
+        ] ,
+
+     ];
+
+     $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post['slug'] === $slug) {
+            $new_post   = $post;
+        }
+    }
+
+
+return view('post',[
+    'title_navbar' => 'Blog',
+    'title' => 'Single Post Page',
+    'post'  => $new_post
+]);
 });
